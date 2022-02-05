7eaf6e04fb29459f5e829436e3a3aa34411e9dbf
TheMole3 <hugomelo04@gmail.com>
2022-02-05 17:04:23 +0100

    Formatting fixes

--- src/rgbLED.cpp
+++ src/rgbLED.cpp
@@ -8,7 +8,8 @@
 #include "Arduino.h"
 #include "rgbLED.h"
 
-rgbLED::rgbLED(int pinR, int pinG, int pinB) {
+rgbLED::rgbLED(int pinR, int pinG, int pinB) 
+{
     _pinR = pinR;
     _pinG = pinG;
     _pinB = pinB;
@@ -18,7 +19,8 @@ rgbLED::rgbLED(int pinR, int pinG, int pinB) {
     pinMode(_pinB, OUTPUT);
 };
 
-void rgbLED::setColor(int R, int G, int B) {
+void rgbLED::setColor(int R, int G, int B) 
+{
     analogWrite(_pinR, R);
     analogWrite(_pinG, G);
     analogWrite(_pinB, B);
