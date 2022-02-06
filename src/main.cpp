ad5fea04096cc19498afa128680d835dd4a53f61
TheMole3 <hugomelo04@gmail.com>
2022-02-06 22:01:17 +0100

    Started work on main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 
+#include "CrustiLocker.h"
+
+CrustiLocker crustiLocker;
+
 void setup() 
 {
-
+    Serial.begin(9600);
+    
+    crustiLocker.setup();
 }
 
 // the loop function runs over and over again forever
 void loop() 
 {
+    crustiLocker.loop();
 }
