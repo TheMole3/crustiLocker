3fbbc1308ed8616b5832b84aca2b6a01a249f950
TheMole3 <hugomelo04@gmail.com>
2022-02-06 19:58:53 +0100

    Added network communication

---
+++ include/network.h
@@ -0,0 +1,32 @@
+/*
+
+  network.h - Library for using ESP network features
+
+*/
+
+#ifndef network_h
+#define network_h
+
+#include "Arduino.h"
+
+#include <string>
+
+#include <ESP8266WiFi.h>        // Include the Wi-Fi library
+#include <ESP8266HTTPClient.h>  // Include the request library
+#include <ArduinoJson.h>        // Include json library
+
+class network 
+{
+  public:
+    void init();
+
+    String httpGETRequest(const char* serverName, String access_token="");
+    String httpPOSTRequest(const char* serverName, String body, String access_token="");
+
+  private:
+    const char* ssid        = "CrustiLocker";         // The SSID (name) of the Wi-Fi network you want to connect to
+    const char* password    = "CrustiLocker2022";     // The password of the Wi-Fi network
+};
+
+
+#endif
---
+++ src/network.cpp
@@ -0,0 +1,111 @@
+/*
+
+  network.h - Library for using ESP network features
+
+*/
+
+#include "Arduino.h"
+#include "network.h"
+
+
+void network::init()
+{
+    // Setting ESP into STATION mode only (no AP mode or dual mode)
+    wifi_set_opmode(STATION_MODE);
+    
+    WiFi.begin(ssid, password);             // Connect to the network
+    Serial.print("\n\nConnecting to ");
+    Serial.println(ssid);
+
+    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+        delay(1000);
+        Serial.print('.');
+    }
+
+    Serial.println('\n');
+    Serial.println("Connection established!");  
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+    Serial.print("\n");
+};
+
+
+String network::httpGETRequest(const char* serverName, String access_token) 
+{
+    Serial.println("GET " + String(serverName));
+
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient http;
+        
+    // Your IP address with path or Domain name with URL path 
+    http.begin(client, serverName);
+
+    // Add auth header
+    if(access_token) http.addHeader("Authorization", "Token " + access_token, true);
+    
+    // Send HTTP POST request
+    int httpResponseCode = http.GET();
+    
+    String payload = "{}"; 
+    
+    if (httpResponseCode>0) 
+    {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString();
+    }
+    else 
+    {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+
+        // TODO send error to pushbullet
+    }
+    // Free resources
+    http.end();
+
+    return payload;
+}
+
+String network::httpPOSTRequest(const char* serverName, String body, String access_token) 
+{
+    Serial.println("POST " + String(serverName));
+
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient http;
+
+    // Your IP address with path or Domain name with URL path 
+    http.begin(client, serverName);
+
+    // Add auth header
+    if(access_token) http.addHeader("Authorization", "Token " + access_token, true);
+    
+    http.addHeader("Content-Type", "application/json");
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(body);
+    
+    String payload = "{}"; 
+    
+    if (httpResponseCode>0) 
+    {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        payload = http.getString();
+    }
+    else 
+    {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+
+        // TODO send error to pushbullet
+    }
+    // Free resources
+    http.end();
+
+    return payload;
+}
