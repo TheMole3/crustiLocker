e75e007cc59fc94aceee8d6bfed6e9552547d170
Hugo Arnlund <hugomelo04@gmail.com>
2022-11-23 10:47:29 +0100

    Added blinking to led

--- include/rgbLED.h
+++ include/rgbLED.h
@@ -14,12 +14,24 @@ class RGBLED
   public:
     RGBLED(int pinR, int pinG, int pinB);
     void setColor(int R, int G, int B);
+    void blink(int R, int G, int B, int ms);
+    void stopBlink();
+    void loop();
 
   private:
     int _pinR;
     int _pinG;
     int _pinB;
-};
 
+    bool isBlinking;
+    bool bl1;
+
+    int R;
+    int G;
+    int B;
+
+    int ms;
+    unsigned long nextBlink;
+};
 
 #endif
--- src/rgbLED.cpp
+++ src/rgbLED.cpp
@@ -24,4 +24,44 @@ void RGBLED::setColor(int R, int G, int B)
     analogWrite(_pinR, R);
     analogWrite(_pinG, G);
     analogWrite(_pinB, B);
-};
+}
+
+void RGBLED::blink(int R, int G, int B, int ms)
+{
+    this->R = R;
+    this->G = G;
+    this->B = B;
+    this->ms = ms;
+
+    isBlinking = true;
+    nextBlink = millis() + ms;
+
+    setColor(R, G, B);
+    bl1 = false;
+}
+
+void RGBLED::stopBlink()
+{
+    isBlinking = false;
+    setColor(0, 0, 0);
+}
+
+void RGBLED::loop()
+{
+    // Wait until next blink
+    if(millis() >= nextBlink && isBlinking) 
+    {
+        nextBlink = millis() + ms;
+
+        if(bl1) 
+        {
+            setColor(R, G, B);
+        }
+        else
+        {
+            setColor(0, 0, 0);
+        }
+
+        bl1 = bl1;
+    }
+}
